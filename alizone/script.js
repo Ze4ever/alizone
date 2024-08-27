b9662cf223e83e2f2af3379209e1190c4d9ee298
Ze4ever <zemario127@gmail.com>
2024-08-27 18:12:17 +0100

    Delete alizone directory

--- alizone/script.js
+++
@@ -1,6 +0,0 @@
-// Código JavaScript opcional para funcionalidades adicionais
-document.querySelectorAll('.product-item img').forEach(img => {
-    img.addEventListener('click', () => {
-        alert('Imagem do produto clicada!');
-    });
-});
